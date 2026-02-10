f22b8563f5aede912dc1edd23c01b12854b2725f
Muhammad Rizki Ardhana <marsharizky01@gmail.com>
2026-02-11 03:56:11 +0700

    fix: add environment variables and fix API endpoints for categories and products

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "be-sporton.agunacourse.com",
+        pathname: "/uploads/**",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
